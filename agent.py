207a21cffd3dd312429e852db4a3068f5ccd848b
Ellie2020 <elena.lestini@gmail.com>
2022-06-18 12:12:01 +0100

    RL first commit

---
+++ agent.py
@@ -0,0 +1,12 @@
+class Agent(object):
+    def __init__(self):
+        self.state_history = None
+
+    def choose_action(self):
+        pass
+
+    def update_state_history(self):
+        pass
+
+    def learn(self):
+        pass
